6e4c116a073bba54db7b6eacf6ed225c4a216d02
Martin Geisse <martingeisse@googlemail.com>
2024-08-04 08:28:41 +0200

    move unit tests for record converter to the same package as the converter itself

--- grumpyjson-core/src/test/java/io/github/grumpystuff/grumpyjson/builtin/record/RecordWithOptionalPropertyConverterTest.java
+++ grumpyjson-core/src/test/java/io/github/grumpystuff/grumpyjson/builtin/record/RecordWithOptionalPropertyConverterTest.java
@@ -4,8 +4,10 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
-package io.github.grumpystuff.grumpyjson;
+package io.github.grumpystuff.grumpyjson.builtin.record;
 
+import io.github.grumpystuff.grumpyjson.JsonRegistries;
+import io.github.grumpystuff.grumpyjson.JsonTestUtil;
 import io.github.grumpystuff.grumpyjson.builtin.primitive.IntegerConverter;
 import io.github.grumpystuff.grumpyjson.builtin.primitive.StringConverter;
 import io.github.grumpystuff.grumpyjson.builtin.helper_types.OptionalField;
