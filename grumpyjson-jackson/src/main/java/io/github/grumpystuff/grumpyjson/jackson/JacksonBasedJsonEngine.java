7c83dfce394f5dfee89c64d9b5da630a3c5ecd1b
Martin Geisse <martin.geisse@janitza.de>
2024-10-09 14:13:05 +0200

    rename Java package in jackson module from "gson" to "jackson"

--- grumpyjson-jackson/src/main/java/io/github/grumpystuff/grumpyjson/jackson/JacksonBasedJsonEngine.java
+++ grumpyjson-jackson/src/main/java/io/github/grumpystuff/grumpyjson/jackson/JacksonBasedJsonEngine.java
@@ -4,7 +4,7 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
-package io.github.grumpystuff.grumpyjson.gson;
+package io.github.grumpystuff.grumpyjson.jackson;
 
 
 import com.fasterxml.jackson.core.JsonParseException;
