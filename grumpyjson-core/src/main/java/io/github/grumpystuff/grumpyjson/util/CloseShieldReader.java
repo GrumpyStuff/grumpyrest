cbcb7999215ebfcda8241d20f6f6e7edc354446e
Martin Geisse <martin.geisse@janitza.de>
2024-10-10 09:08:17 +0200

    javadoc

--- grumpyjson-core/src/main/java/io/github/grumpystuff/grumpyjson/util/CloseShieldReader.java
+++ grumpyjson-core/src/main/java/io/github/grumpystuff/grumpyjson/util/CloseShieldReader.java
@@ -5,54 +5,129 @@ import java.io.Reader;
 import java.io.Writer;
 import java.nio.CharBuffer;
 
+/**
+ * NOT PUBLIC API
+ */
 public final class CloseShieldReader extends Reader {
 
     private final Reader in;
 
+    /**
+     * NOT PUBLIC API
+     *
+     * @param in ...
+     */
     public CloseShieldReader(Reader in) {
         this.in = in;
     }
 
+    /**
+     * NOT PUBLIC API
+     *
+     * @param target ...
+     * @return ...
+     * @throws IOException ...
+     */
     public int read(CharBuffer target) throws IOException {
         return in.read(target);
     }
 
+    /**
+     * NOT PUBLIC API
+     *
+     * @return ...
+     * @throws IOException ...
+     */
     public int read() throws IOException {
         return in.read();
     }
 
+    /**
+     * NOT PUBLIC API
+     *
+     * @param cbuf ...
+     * @return ...
+     * @throws IOException ...
+     */
     public int read(char[] cbuf) throws IOException {
         return in.read(cbuf);
     }
 
+    /**
+     * NOT PUBLIC API
+     *
+     * @param cbuf ...
+     * @param off ...
+     * @param len ...
+     * @return ...
+     * @throws IOException ...
+     */
     public int read(char[] cbuf, int off, int len) throws IOException {
         return in.read(cbuf, off, len);
     }
 
+    /**
+     * NOT PUBLIC API
+     *
+     * @param n ...
+     * @return ...
+     * @throws IOException ...
+     */
     public long skip(long n) throws IOException {
         return in.skip(n);
     }
 
+    /**
+     * NOT PUBLIC API
+     *
+     * @throws IOException ...
+     */
     public boolean ready() throws IOException {
         return in.ready();
     }
 
+    /**
+     * NOT PUBLIC API
+     *
+     * @return ...
+     */
     public boolean markSupported() {
         return in.markSupported();
     }
 
+    /**
+     * NOT PUBLIC API
+     *
+     * @param readAheadLimit ...
+     * @throws IOException ...
+     */
     public void mark(int readAheadLimit) throws IOException {
         in.mark(readAheadLimit);
     }
 
+    /**
+     * NOT PUBLIC API
+     *
+     * @throws IOException ...
+     */
     public void reset() throws IOException {
         in.reset();
     }
 
+    /**
+     * NOT PUBLIC API
+     */
     public void close() {
         // do nothing
     }
 
+    /**
+     * NOT PUBLIC API
+     *
+     * @param out ...
+     * @return ...
+     * @throws IOException ...
+     */
     public long transferTo(Writer out) throws IOException {
         return in.transferTo(out);
     }
--- grumpyjson-core/src/main/java/io/github/grumpystuff/grumpyjson/util/CloseShieldWriter.java
+++ grumpyjson-core/src/main/java/io/github/grumpystuff/grumpyjson/util/CloseShieldWriter.java
@@ -3,50 +3,120 @@ package io.github.grumpystuff.grumpyjson.util;
 import java.io.IOException;
 import java.io.Writer;
 
+/**
+ * NOT PUBLIC API
+ */
 public final class CloseShieldWriter extends Writer {
 
     private final Writer out;
 
+    /**
+     * NOT PUBLIC API
+     *
+     * @param out ...
+     */
     public CloseShieldWriter(Writer out) {
         this.out = out;
     }
 
+    /**
+     * NOT PUBLIC API
+     *
+     * @param c ...
+     * @throws IOException ...
+     */
     public void write(int c) throws IOException {
         out.write(c);
     }
 
+    /**
+     * NOT PUBLIC API
+     *
+     * @param cbuf ...
+     * @throws IOException ...
+     */
     public void write(char[] cbuf) throws IOException {
         out.write(cbuf);
     }
 
+    /**
+     * NOT PUBLIC API
+     *
+     * @param cbuf ...
+     * @param off ...
+     * @param len ...
+     * @throws IOException ...
+     */
     public void write(char[] cbuf, int off, int len) throws IOException {
         out.write(cbuf, off, len);
     }
 
+    /**
+     * NOT PUBLIC API
+     *
+     * @param str ...
+     * @throws IOException ...
+     */
     public void write(String str) throws IOException {
         out.write(str);
     }
 
+    /**
+     * NOT PUBLIC API
+     *
+     * @param str ...
+     * @param off ...
+     * @param len ...
+     * @throws IOException ...
+     */
     public void write(String str, int off, int len) throws IOException {
         out.write(str, off, len);
     }
 
+    /**
+     * NOT PUBLIC API
+     *
+     * @param csq ...
+     * @throws IOException ...
+     */
     public Writer append(CharSequence csq) throws IOException {
         return out.append(csq);
     }
 
+    /**
+     * NOT PUBLIC API
+     *
+     * @param csq ...
+     * @param start ...
+     * @param end ...
+     * @throws IOException ...
+     */
     public Writer append(CharSequence csq, int start, int end) throws IOException {
         return out.append(csq, start, end);
     }
 
+    /**
+     * NOT PUBLIC API
+     *
+     * @param c ...
+     * @throws IOException ...
+     */
     public Writer append(char c) throws IOException {
         return out.append(c);
     }
 
+    /**
+     * NOT PUBLIC API
+     *
+     * @throws IOException ...
+     */
     public void flush() throws IOException {
         out.flush();
     }
 
+    /**
+     * NOT PUBLIC API
+     */
     public void close() {
         // do nothing
     }
